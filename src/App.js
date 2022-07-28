569f5aaf602871412faaaa70726663b5227d40bb
Muskan <muskanrangnani22@gmail.com>
2022-07-28 14:42:09 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Expenses from "./components/Expenses";
+
+function App() {
+ 
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses />
+
+
+    </div >
+  );
+}
+
+export default App;
+  
